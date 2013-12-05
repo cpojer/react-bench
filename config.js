8b36b9fed35a91c7e9359cd4883f3da54f179873
cpojer <christoph.pojer@gmail.com>
2013-12-05 12:55:13 -0800

    Update for React >= 0.5

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 var FRAMEWORK_SOURCES = [
   '//cdnjs.cloudflare.com/ajax/libs/react/0.5.1/react.min.js',
-  '//cdnjs.cloudflare.com/ajax/libs/react/0.4.1/react.min.js',
+  '//localhost:8888/react.min.old.js',
+  '//localhost:8888/react/build/react.min.js'
 ];
